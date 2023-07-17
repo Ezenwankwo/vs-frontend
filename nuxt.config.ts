61b99844d7efe479a739a450ac4ebd74ceff5d05
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2023-07-17 15:04:50 +0100

    config tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['nuxt-gtag'],
-
-  gtag: {
-    id: 'G-QKF4WH7SYY'
-  }
+  modules: [
+    'nuxt-gtag',
+    '@nuxtjs/tailwindcss',
+  ],
+  
+  gtag: { id: 'G-QKF4WH7SYY' },
 })
