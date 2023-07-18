15c5f374ca78d643fe74921a96db312064a8794a
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2023-07-18 09:45:05 +0100

    configured nuxt eslint module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['nuxt-gtag', '@nuxtjs/tailwindcss'],
+  modules: [
+    'nuxt-gtag',
+    '@nuxtjs/tailwindcss',
+    ['@nuxtjs/eslint-module', { lintOnStart: false }],
+  ],
   gtag: { id: 'G-QKF4WH7SYY' },
 })
