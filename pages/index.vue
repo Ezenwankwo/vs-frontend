932c17dbbfcf76f77e51ff5b319f59ea5efa95a8
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2023-11-14 09:01:55 +0100

    config daisyui

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,5 @@
 <template>
   <div class="mx-auto">
-    <div height="800px" width="1200px">
-      <iframe
-        id="waitlist_iframe"
-        allowtransparency="true"
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
-        width="1200px"
-        height="800px"
-        src="https://getwaitlist.com/waitlist/8879"
-        style="border-radius: 8px"
-      />
-    </div>
-    <component :is="'script'" async>
-      const url = new URL(window.location.href); const ref_id =
-      url.searchParams.get('ref_id'); if (ref_id) {
-      document.getElementById("waitlist_iframe").src += `?ref_id=${ref_id}`; }
-    </component>
+    <NuxtWelcome />
   </div>
 </template>
---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#D2AE6D',
+          secondary: '#FFD700',
+          accent: '#EBDDC6',
+          neutral: '#FAF7F1',
+          'base-100': '#ffffff',
+        },
+      },
+    ],
+  },
+}
