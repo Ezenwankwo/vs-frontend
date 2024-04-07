cb093c0e5df2b09f7ea9df8f789858e57da48487
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2024-04-07 10:29:07 +0100

    test map

--- pages/apartments/[slug].vue
+++ pages/apartments/[slug].vue
@@ -1,10 +1,20 @@
 <template>
   <UContainer class="mb-24">
     <UBreadcrumb divider="/" :links="breadCrumbLinks" class="my-4 lg:my-6" />
-    <div class="flex">
-      <NuxtImg src="/shortlet.png" class="object-cover h-96 rounded-md w-2/3" />
-      <div class="w-1/3">Map</div>
+    <div class="flex space-x-4">
+      <NuxtImg src="/shortlet.png" class="object-cover h-96 rounded-md w-3/5" />
+      <div class="w-2/5"></div>
     </div>
+    <iframe
+      width="600"
+      height="450"
+      style="border: 0"
+      allowfullscreen
+      referrerpolicy="no-referrer-when-downgrade"
+      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyADo33Dod-kr1-zUHXiPGUEnkmCPT7LWwE
+    &q=Space+Needle,Seattle+WA"
+    >
+    </iframe>
   </UContainer>
 </template>
 
